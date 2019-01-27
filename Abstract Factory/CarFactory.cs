f649ffc710462653e3bf9da6370ce6f86b5baadd
hassan olfat <hassan.olfat@LIVE.COM>
2019-01-27 06:45:01 +0330

    Add project files.

---
+++ Abstract Factory/CarFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_Factory
+{
+     public enum Carsname
+    {
+        Volvo ,
+        Benz
+    }
+    public interface ICar
+    {
+        string GetCarPrice();
+
+    }
+
+    public abstract class CarFactory
+    {
+        public static ICar GetCar(Carsname carName)
+        {
+            switch (carName)
+            {
+                case Carsname.Volvo:
+                {
+                    return new Volvo();
+                }
+                case Carsname.Benz:
+                {
+                    return new Benz();
+                }
+                default:
+                    return null;
+
+            }
+        }
+    }
+    class Volvo : ICar
+    {
+        public string GetCarPrice()
+        {
+            return "$100";
+        }
+    }
+
+    class Benz : ICar
+    {
+        public string GetCarPrice()
+        {
+            return "$200";
+        }
+    }
+}
---
+++ Abstract Factory/bicycleFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_Factory
+{
+    public enum Bicyclesname
+    {
+        Volvo,
+        Benz
+    }
+    public interface Ibicycle
+    {
+        string GetbicyclePrice();
+
+    }
+
+    public abstract class BicycleFactory
+    {
+        public static Ibicycle GetBicycle(Bicyclesname carName)
+        {
+            switch (carName)
+            {
+                case Bicyclesname.Volvo:
+                {
+                    return new Trek();
+                }
+                case Bicyclesname.Benz:
+                {
+                    return new Specialized();
+                }
+                default:
+                    return null;
+
+            }
+        }
+    }
+
+    class Trek : Ibicycle
+    {
+        public string GetbicyclePrice()
+        {
+            return "$100";
+        }
+    }
+
+    class Specialized : Ibicycle
+    {
+        public string GetbicyclePrice()
+        {
+            return "$200";
+        }
+    }
+}
